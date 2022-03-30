e0095c74ff81542f0b1ffe3f251c39eda0f9e7b9
kalokli8 <gelu13579@hotmail.com.hk>
2022-03-30 22:28:15 +0800

    added auth ready

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import { useContext } from "react";
 import AuthContext from "../stores/authContext";
 
 export default function Navbar() {
-  const { user, login, logout } = useContext(AuthContext);
+  const { user, login, logout, authReady } = useContext(AuthContext);
   console.log(user);
 
   return (
@@ -12,31 +12,33 @@ export default function Navbar() {
       <nav>
         <Image src="/rupee.png" width={50} height={48} />
         <h1>Gaming Vibes</h1>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/guides">
-              <a>Guides</a>
-            </Link>
-          </li>
-
-          {user ? (
-            <>
-              <li>{user.emailg}</li>
-              <li onClick={login} className="btn">
-                Log out
-              </li>
-            </>
-          ) : (
-            <li onClick={logout} className="btn">
-              Login/Signup
+        {authReady && (
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
             </li>
-          )}
-        </ul>
+            <li>
+              <Link href="/guides">
+                <a>Guides</a>
+              </Link>
+            </li>
+
+            {user ? (
+              <>
+                <li>{user.emailg}</li>
+                <li onClick={login} className="btn">
+                  Log out
+                </li>
+              </>
+            ) : (
+              <li onClick={logout} className="btn">
+                Login/Signup
+              </li>
+            )}
+          </ul>
+        )}
       </nav>
       <div className="banner">
         <Image src="/banner.png" width={966} height={276} />
